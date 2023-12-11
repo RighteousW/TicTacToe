fb6c973a3b34c771fdebfd3d094d084cab2df45d
RighteousW <140548434+RighteousW@users.noreply.github.com>
2023-12-11 08:33:50 +0200

    made it work

--- src/Game.java
+++ src/Game.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Game {
     private static final int BOARD_SIZE = 3;        // size of the board, it's a square
     private static final char[] SYMBOLS = {'O','X'};
-    char[][] board = new char[BOARD_SIZE][BOARD_SIZE];      // the board of the game
+    char[] board = new char[BOARD_SIZE*BOARD_SIZE];      // the board of the game
     int player1_wins;      // number of wins of player using 'O' if on loop
     int player2_wins;      // number of wins of player using 'X' if on loop
 
@@ -14,66 +14,64 @@ public class Game {
     }
     // checking if the place where you want to place move is valid
     public boolean is_valid_move(int move){
-        if(0 > move || move > 8){
+        if(0 > move || move > BOARD_SIZE*BOARD_SIZE-1){
             return false;
         }
-        return board[move /BOARD_SIZE][move %BOARD_SIZE] == '\0';
+        return board[move] == '\0';
     }
     public boolean can_not_play_move(int move, char letter){
         if(is_valid_move(move)){
-            board[move /BOARD_SIZE][move %BOARD_SIZE] = letter;
+            board[move] = letter;
             return false;
         }
         return true;
     }
     public String game_string(){
         StringBuilder result = new StringBuilder();
-        for(int i = 0; i < BOARD_SIZE; i++){
-            for(int j = 0; j < BOARD_SIZE; j++){
-                result.append("|").append(this.board[i][j]).append("|");
-            }
-            result.append('\n');
+        for(int i = 0; i < BOARD_SIZE*BOARD_SIZE; i++){
+            result.append("|").append(this.board[i]).append("|");
+            if(i != 0 && (i+1) % BOARD_SIZE == 0)
+                result.append('\n');
         }
         return result.toString();
     }
     public String board_numbers(){
         StringBuilder result = new StringBuilder();
-        for(int i = 0; i < BOARD_SIZE; i++){
-            for(int j = 0; j < BOARD_SIZE; j++){
-                result.append("|").append(i +j).append("|");
-            }
-            result.append('\n');
+        for(int i = 0; i < BOARD_SIZE*BOARD_SIZE; i++){
+            result.append("|").append(i).append("|");
+            if(i != 0 && (i+1) % BOARD_SIZE == 0)
+                result.append('\n');
         }
         return result.toString();
     }
-    public char check_winner(){
+    public boolean is_winning_move(){
         for (char symbol : SYMBOLS) {
-            if (board[0][0] == board[1][0] && board[1][0] == board[2][0] && board[2][0] == symbol) {
-                return symbol;
+            if (board[0] == board[1] && board[1] == board[2] && board[2] == symbol) {
+                return true;
             }
-            if (board[0][1] == board[1][1] && board[1][1] == board[2][1] && board[2][1] == symbol) {
-                return symbol;
+            if (board[3] == board[4] && board[4] == board[5] && board[5] == symbol) {
+                return true;
             }
-            if (board[0][2] == board[1][2] && board[1][2] == board[2][2] && board[2][2] == symbol) {
-                return symbol;
+            if (board[6] == board[7] && board[7] == board[8] && board[8] == symbol) {
+                return true;
             }
-            if (board[0][0] == board[0][1] && board[0][1] == board[0][2] && board[0][2] == symbol) {
-                return symbol;
+            if (board[0] == board[3] && board[3] == board[6] && board[6] == symbol) {
+                return true;
             }
-            if (board[1][0] == board[1][1] && board[1][1] == board[1][2] && board[1][2] == symbol) {
-                return symbol;
+            if (board[1] == board[4] && board[4] == board[7] && board[7] == symbol) {
+                return true;
             }
-            if (board[2][0] == board[2][1] && board[2][1] == board[2][2] && board[2][2] == symbol) {
-                return symbol;
+            if (board[2] == board[5] && board[5] == board[8] && board[8] == symbol) {
+                return true;
             }
-            if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[2][2] == symbol) {
-                return symbol;
+            if (board[0] == board[4] && board[4] == board[8] && board[8] == symbol) {
+                return true;
             }
-            if (board[2][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] == symbol) {
-                return symbol;
+            if (board[2] == board[4] && board[4] == board[6] && board[6] == symbol) {
+                return true;
             }
         }
-        return '9';
+        return false;
     }
     public String score(){
         return "Scores: \n" +
@@ -95,7 +93,7 @@ public class Game {
             do{
                 move = scanner.nextInt();
             }while(can_not_play_move(move, player1.getLetter()));
-            if(check_winner() != '\0'){
+            if(is_winning_move()){
                 System.out.println(player1 + " wins");
                 this.player1_wins ++;
                 System.out.println(score());
@@ -107,7 +105,7 @@ public class Game {
             do{
                 move = scanner.nextInt();
             }while(can_not_play_move(move, player2.getLetter()));
-            if(check_winner() != '\0'){
+            if(is_winning_move()){
                 System.out.println(player2 + " wins");
                 this.player2_wins ++;
                 System.out.println(score());
